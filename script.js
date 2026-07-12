7eca3beae7aa84122c42648face428b23cd310f2
Utkarsh Verma <utkarsh1765uv7@gmail.com>
2026-07-13 03:33:51 +0530

    added mute/unmute button for the video

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let isMuted=true;
-const siteName="Utkarsh's Portfolio";
-let clickCount=0;
-console.log(isMuted);
-console.log(siteName);
-console.log(clickCount);
+const muteButton = document.getElementById("mute-btn");
+const video = document.querySelector(".profile-pic");
+muteButton.addEventListener("click", function () {
+    video.muted = !video.muted;
+    const icon=muteButton.querySelector("i");//here i is searched inside the #mute-btn id(used in muteButton) this is why we have used muteButton here
+    if (video.muted)
+        icon.className="fa-solid fa-volume-xmark";
+    else
+        icon.className="fa-solid fa-volume-high";
+});
