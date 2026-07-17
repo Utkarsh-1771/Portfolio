7a467dbe942ecbf34149ae8df2bd5b22737448cd
Utkarsh Verma <utkarsh1765uv7@gmail.com>
2026-07-18 04:15:56 +0530

    started working on js again

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const muteButton = document.getElementById("mute-btn");
 const video = document.querySelector(".profile-pic");
-muteButton.addEventListener("click", function () {
+function toggleMute() {
     video.muted = !video.muted;
     const icon=muteButton.querySelector("i");//here i is searched inside the #mute-btn id(used in muteButton) this is why we have used muteButton here
     if (video.muted)
         icon.className="fa-solid fa-volume-xmark";
     else
         icon.className="fa-solid fa-volume-high";
-});
+}
+muteButton.addEventListener("click",toggleMute);
